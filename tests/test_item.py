07c099f4349bb993bdecc86ed0e0eeb3c601a6ea
artem <artemshmoylov123@gmail.com>
2023-06-06 17:19:08 +0300

    Написаны тесты на класс Item. 90% покрытие

--- tests/test_item.py
+++ tests/test_item.py
@@ -18,9 +18,27 @@ def test_init(test_item):
 def test_calculate_total_price(test_item):
     assert test_item.calculate_total_price() == 8400000
 
+
 def test_apply_discount(test_item):
     test_item.apply_discount()
     assert test_item.price == 600000.0
     test_item.pay_rate = 0.5
     test_item.apply_discount()
     assert test_item.price == 300000.0
+
+
+def test_name_setter(test_item):
+    test_item.name = 'Экран'
+    assert test_item.name == "Экран"
+    with pytest.raises(Exception):
+        test_item.name = "Микроволновка"
+
+
+def test_instantiate_from_csv(test_item):
+    assert Item.instantiate_from_csv() is None
+
+def test_string_to_number():
+    assert Item.string_to_number("6.5") == 6
+    assert Item.string_to_number(6.5) == 6
+    with pytest.raises(ValueError):
+        Item.string_to_number("пять")
